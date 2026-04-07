3b2d130568ec16df420a29c4cfeb8cbab777159c
Arlene Riona <60304739@udst.edu.qa>
2026-04-07 20:21:08 +0300

    feat: update train.py with optimal hyperparameters from sweep job

--- src/train.py
+++ src/train.py
@@ -18,8 +18,8 @@ def parse_args():
     parser.add_argument("--val_data", type=str, required=True)
     parser.add_argument("--test_data", type=str, required=True)
     parser.add_argument("--output", type=str, required=True)
-    parser.add_argument("--C", type=float, default=1.0)
-    parser.add_argument("--max_iter", type=int, default=200)
+    parser.add_argument("--C", type=float, default=0.3213738590686788)
+    parser.add_argument("--max_iter", type=int, default=100)
     return parser.parse_args()
 
 
